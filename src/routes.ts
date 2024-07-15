0548ec4d44b613999539251fc046652d1d0b00d0
Talles Amaral <97260722+amxraltls@users.noreply.github.com>
2024-07-14 22:48:37 -0300

    Rota auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from 'express';
 import CreateUserController from './controllers/user/CreateUserController';
+import AuthUserCnotroller from './controllers/user/AuthUserController';
 
 const router = Router();
 
 router.post('/users', CreateUserController.handle);
+router.post('/session', AuthUserCnotroller.handle);
 
 export { router };
